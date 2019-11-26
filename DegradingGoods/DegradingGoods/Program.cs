9079d2943dfa654634ba3c99b6cf632469b4c60c
Josh Birks <joshbs06@hotmail.com>
2019-11-26 12:53:44 +0000

    completed degrading logic for most sections

--- DegradingGoods/DegradingGoods/Program.cs
+++ DegradingGoods/DegradingGoods/Program.cs
@@ -10,30 +10,49 @@ namespace DegradingGoods
     class Program
     {
 
+        static int QualityDegradingLogic(Item item, int currentDegradingAmount) {
+
+            if (item.SellIn < 0) {
+                return currentDegradingAmount * 2;
+            }
+
+            return currentDegradingAmount;
+        }
+
         static Item DegradationMethod(int degradationAmount, Item item) {
 
+            var degradeAmount = 0;
             var degradedItem = new Item();
             degradedItem = item;
             for (var i = 0; i < degradationAmount; i++) {
+                if (item.Name.ToLower() == "aged brie")
+                {
+                    degradeAmount = QualityDegradingLogic(degradedItem, 1);
+                    degradedItem.SellIn = degradedItem.SellIn - 1;
+                    degradedItem.QualityValue = degradedItem.QualityValue + degradeAmount;
+                }
+                else if (item.Name.ToLower() == "sulfuras")
+                {
+                    return degradedItem;
+                }
+                else if (degradedItem.Name.ToLower() == "conjured")
+                {
+                    degradeAmount = QualityDegradingLogic(degradedItem,-2);
+                    degradedItem.SellIn = degradedItem.SellIn - 1;
+                    degradedItem.QualityValue = degradedItem.QualityValue - degradeAmount;
+                }
+                else if (degradedItem.Name.ToLower() == "backstage passes")
+                {
 
-            }
-
-            if (item.Name.ToLower() == "aged brie") {
-                degradedItem.SellIn = degradedItem.SellIn - 1;
-                degradedItem.QualityValue = degradedItem.QualityValue + 1;
-            }
-            if (item.Name.ToLower() == "sulfuras")
-            {
-                return degradedItem;
-            }
-            if (degradedItem.Name.ToLower() == "conjured") {
-                degradedItem.SellIn = degradedItem.SellIn - 1;
-                degradedItem.QualityValue = degradedItem.QualityValue + 2;
-            }
-            if (degradedItem.Name.ToLower() == "backstage passes") {
-                
-            }
-                       
+                }
+                else
+                {
+                    degradeAmount = QualityDegradingLogic(degradedItem, -1);
+                    degradedItem.SellIn = degradedItem.SellIn - 1;
+                    degradedItem.QualityValue = degradedItem.QualityValue - degradeAmount;
+                }
+            };
+                               
             return degradedItem;
         }
         
@@ -41,25 +60,58 @@ namespace DegradingGoods
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
 
             var item = new Item();
             var daysOfDegradation = 0;
 
-            //Gains Item Input//
-            Console.WriteLine("Enter item Name followed by enter");
-            item.Name = Console.ReadLine();
-            Console.WriteLine("Enter item Sell In Value followed by enter");
-            item.SellIn = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter item Quality Value followed by enter");
-            item.QualityValue = Convert.ToInt32(Console.ReadLine());
+            var itemList = new List<Item>()
+            {
+                new Item(){
+                    Name = "Aged Brie",
+                    SellIn = 1,
+                    QualityValue = 1
+                }
+            };
 
-            //Gains days of degradation wanted//
-            Console.WriteLine("Please enter how many days of degrading wanted");
-            daysOfDegradation = Convert.ToInt32(Console.ReadLine());
+            var degradedItemList = new List<Item>();
+                       
+            foreach (var itemListElement in itemList)
+            {
+                if (itemListElement.Name.ToUpper() == "INVALID ITEM")
+                {
+                    var invalidItem = new Item()
+                    {
+                        Name = "NO SUCH ITEM",
+                        SellIn = 0,
+                        QualityValue = 0
+                    };
+                    degradedItemList.Add(invalidItem);
+                    Console.WriteLine(invalidItem.Name);
+                }
+
+                var itemToAdd = DegradationMethod(1, itemListElement);
+                degradedItemList.Add(itemToAdd);
+                Console.WriteLine(itemToAdd.Name + ' ' + itemToAdd.SellIn + ' ' + itemToAdd.QualityValue);
+            }
+
+            Console.ReadKey();
+
+
+            //User Input Option
+            ////Gains Item Input//
+            //Console.WriteLine("Enter item Name followed by enter");
+            //item.Name = Console.ReadLine();
+            //Console.WriteLine("Enter item Sell In Value followed by enter");
+            //item.SellIn = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter item Quality Value followed by enter");
+            //item.QualityValue = Convert.ToInt32(Console.ReadLine());
+
+            ////Gains days of degradation wanted//
+            //Console.WriteLine("Please enter how many days of degrading wanted");
+            //daysOfDegradation = Convert.ToInt32(Console.ReadLine());
 
         }
     }
