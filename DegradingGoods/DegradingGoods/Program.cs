8161f4bb85564a961a633ce7634df7038eaf877e
Josh Birks <joshbs06@hotmail.com>
2019-11-26 12:25:51 +0000

    added basic degradation logic and item sorting

--- DegradingGoods/DegradingGoods/Program.cs
+++ DegradingGoods/DegradingGoods/Program.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DegradingGoods;
 
 namespace DegradingGoods
 {
     class Program
     {
+
+        static Item DegradationMethod(int degradationAmount, Item item) {
+
+            var degradedItem = new Item();
+            degradedItem = item;
+            for (var i = 0; i < degradationAmount; i++) {
+
+            }
+
+            if (item.Name.ToLower() == "aged brie") {
+                degradedItem.SellIn = degradedItem.SellIn - 1;
+                degradedItem.QualityValue = degradedItem.QualityValue + 1;
+            }
+            if (item.Name.ToLower() == "sulfuras")
+            {
+                return degradedItem;
+            }
+            if (degradedItem.Name.ToLower() == "conjured") {
+                degradedItem.SellIn = degradedItem.SellIn - 1;
+                degradedItem.QualityValue = degradedItem.QualityValue + 2;
+            }
+            if (degradedItem.Name.ToLower() == "backstage passes") {
+                
+            }
+                       
+            return degradedItem;
+        }
+        
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
@@ -16,6 +45,22 @@ namespace DegradingGoods
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            var item = new Item();
+            var daysOfDegradation = 0;
+
+            //Gains Item Input//
+            Console.WriteLine("Enter item Name followed by enter");
+            item.Name = Console.ReadLine();
+            Console.WriteLine("Enter item Sell In Value followed by enter");
+            item.SellIn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter item Quality Value followed by enter");
+            item.QualityValue = Convert.ToInt32(Console.ReadLine());
+
+            //Gains days of degradation wanted//
+            Console.WriteLine("Please enter how many days of degrading wanted");
+            daysOfDegradation = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
